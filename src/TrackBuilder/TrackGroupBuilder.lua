ff0368c1f377c4615df8c3511db127d69856e95f
Twin1Twin2 <theawesometwin827@gmail.com>
2020-12-25 17:19:06 -0800

    Removed Interval

--- src/TrackBuilder/TrackGroupBuilder.lua
+++ src/TrackBuilder/TrackGroupBuilder.lua
@@ -6,10 +6,6 @@ local BaseSection = require(script.Parent.BaseSection)
 
 local Builder = require(script.Parent.Builder)
 
-local util = script.Parent.Util
-local t = require(util.t)
-
-
 local TrackGroupBuilder = {
 	ClassName = "TrackGroupBuilder";
 }
@@ -22,27 +18,16 @@ function TrackGroupBuilder.new()
 	local self = setmetatable(Builder.new(), TrackGroupBuilder)
 
 	self.Sections = {}
-	self.Interval = nil
-
 
 	return self
 end
 
--- makes sure everything is setup properly
--- runs an "any" check on the following properties
-local BuilderCheck = Builder.Check({
-	"Interval"
-})
-
 
 function TrackGroupBuilder:Build()
-	assert(BuilderCheck(self))
-
 	return TrackGroup.fromData({
 		Name = self.Name,
 
 		Sections = self.Sections,
-		Interval = self.Interval,
 	})
 end
 
@@ -51,15 +36,6 @@ function TrackGroupBuilder:WithSection(section)
 	assert(BaseSection.IsType(section))
 
 	table.insert(self.Sections, section)
-
-	return self
-end
-
-
-function TrackGroupBuilder:WithInterval(interval)
-	assert(t.numberPositive(interval))
-
-	self.Interval = interval
 
 	return self
 end
