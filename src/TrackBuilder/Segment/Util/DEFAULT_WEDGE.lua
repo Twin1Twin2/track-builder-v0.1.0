7af07965c705284458e970f051148a7683ef9bb7
Twin1Twin2 <theawesometwin827@gmail.com>
2021-01-04 03:54:42 -0800

    Modified Size and set material

--- src/TrackBuilder/Segment/Util/DEFAULT_WEDGE.lua
+++ src/TrackBuilder/Segment/Util/DEFAULT_WEDGE.lua
@@ -1,6 +1,8 @@
 
 local DEFAULT_WEDGE = Instance.new("WedgePart")
 DEFAULT_WEDGE.Anchored = true
+DEFAULT_WEDGE.Size = Vector3.new(0.2, 1, 1)
+DEFAULT_WEDGE.Material = Enum.Material.SmoothPlastic
 DEFAULT_WEDGE.TopSurface = Enum.SurfaceType.Smooth
 DEFAULT_WEDGE.BottomSurface = Enum.SurfaceType.Smooth
 
